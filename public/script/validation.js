321b688fea32133535c873a1c69343d3fa70c6ca
Barathi <barathik899@gmail.com>
2023-10-16 13:09:24 +0530

    updated files on ux by barathi

---
+++ public/script/validation.js
@@ -0,0 +1,19 @@
+// Bootstrap validation script
+(function () {
+    'use strict';  //set of rules that are more restrictive and less forgiving than the default rules.
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
